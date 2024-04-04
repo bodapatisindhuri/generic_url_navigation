2891d8e4fffdacf59b61c905fd0a500204172754
bodapatisindhuri <bodapatisindhuri@gmail.com>
2024-04-04 22:37:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def sindhu(request):
+    return render(request,'sindhu.html')
+
+def janith(request):
+    return render(request,'janith.html')
